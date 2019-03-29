1f286c2372332c74fc0a18c03a9331088f2f4459
Hikmat <asephikmatf@gmail.com>
2019-03-29 10:52:19 +0700

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/src/content`,
+        path: `${__dirname}/content/blog`,
       },
     },
     {
